8013ef92b18d154389c06a26ceaadbe7e3002f6d
James <26239426+JamesTGH@users.noreply.github.com>
2023-04-01 15:44:18 -0500

    changed the update/download url & jquery cdn

--- RaterHubHelper.meta.js
+++ RaterHubHelper.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         RaterHubHelper
-// @version      2.0.0
+// @version      2.1.0
 // @description  RaterHubHelper
 // @author       JamesTGH
 // @match        https://www.raterhub.com/*
-// @updateURL    https://jamestgh.com/RaterHubHelper.meta.js
-// @downloadURL  https://jamestgh.com/RaterHubHelper.user.js
-// @require      https://code.jquery.com/jquery-3.6.4.min.js#sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=
+// @updateURL    https://tm.jamestgh.com/RaterHubHelper.meta.js
+// @downloadURL  https://tm.jamestgh.com/RaterHubHelper.user.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js#sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==
 // ==/UserScript==
--- RaterHubHelper.user.js
+++ RaterHubHelper.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
 // @name         RaterHubHelper
-// @version      2.0.0
+// @version      2.1.0
 // @description  RaterHubHelper
 // @author       JamesTGH
 // @match        https://www.raterhub.com/*
-// @updateURL    https://jamestgh.com/RaterHubHelper.meta.js
-// @downloadURL  https://jamestgh.com/RaterHubHelper.user.js
-// @require      https://code.jquery.com/jquery-3.6.4.min.js#sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=
+// @updateURL    https://tm.jamestgh.com/RaterHubHelper.meta.js
+// @downloadURL  https://tm.jamestgh.com/RaterHubHelper.user.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js#sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==
 // ==/UserScript==
 
 $(document).ready(function() {
+    // $('.ewok-buds-body #ewok-submit-div').css('margin-bottom', '12px !important');
     // change the background colors and text colors for dark mode
-    $('body, .container').css('background-color', '#888888');
-    $('table.report th, table.details th, table.frame th, .ewok-buds-card h2').css({'background-color': '#666666', 'color': '#000000'});
-    $('.ewok-editor-editable-columngroup.with-first-row-headers>tbody>tr:first-child, .ewok-buds-summary-section th, input[type="text"], textarea').css('background-color', '#777777');
-    $('.ewok-buds-card, .ewok-buds-body #ewok-task-form>*:not(.ewok-buds):not(.clear):not(#hidden-fields):not(.not-ewok-buds-card)').css('background-color', '#666666');
+    //$('body, .container').css('background-color', '#888888');
+    //$('table.report th, table.details th, table.frame th, .ewok-buds-card h2').css({'background-color': '#666666', 'color': '#000000'});
+    //$('.ewok-editor-editable-columngroup.with-first-row-headers>tbody>tr:first-child, .ewok-buds-summary-section th, input[type="text"], textarea').css('background-color', '#777777');
+    //$('.ewok-buds-card, .ewok-buds-body #ewok-task-form>*:not(.ewok-buds):not(.clear):not(#hidden-fields):not(.not-ewok-buds-card)').css('background-color', '#666666');
     // check if your on a task webpage or homepage to append the start or end time on the title
     if (sessionStorage.getItem('starttaskTime') !== null && window.location.href.indexOf('?taskIds=') === -1) {
         $('title').append(' | ' + new Date(Number(sessionStorage.getItem('starttaskTime'))).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
@@ -40,8 +41,8 @@ $(document).ready(function() {
         let workedMinutes = Math.abs(new Date(Number(sessionStorage.getItem('starttaskTime'))).getTime() - new Date(Number(sessionStorage.getItem('endtaskTime')))) / 6e4;
         // display the starttaskTime and the total amount of time doing tasks at the bottom next to the buttons
         $("div.button-group").append($('<div>')
-            .append($('<b>').text('Start Task Time: ' + (new Date(Number(sessionStorage.getItem('starttaskTime'))).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })).toString() + ' | '))
-            .append($('<b>').text('Total Task Time: ' + Math.floor(workedMinutes / 60) + ':' + (workedMinutes % 60).toString().padStart(2, '0') + ' (' + (workedMinutes / 60).toFixed(2) + ')'))
+            .append($('<b>').text('Started Working Time: ' + (new Date(Number(sessionStorage.getItem('starttaskTime'))).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })).toString() + ' | '))
+            .append($('<b>').text('Total Worked Time: ' + Math.floor(workedMinutes / 60) + ':' + (workedMinutes % 60).toString().padStart(2, '0') + ' (' + (workedMinutes / 60).toFixed(2) + ')'))
         );
         // change the background color if you are within 10 seconds of the endtaskTime
         setInterval(function() {
