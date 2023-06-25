8733efd1d790e7905dd82fdc9c068261c0ead850
James <26239426+JamesTGH@users.noreply.github.com>
2023-06-25 06:12:39 -0500

    Changed jQuery CDN

--- RaterHubHelper.user.js
+++ RaterHubHelper.user.js
@@ -6,7 +6,7 @@
 // @match        https://www.raterhub.com/*
 // @updateURL    https://tm.jamestgh.com/RaterHubHelper.meta.js
 // @downloadURL  https://tm.jamestgh.com/RaterHubHelper.user.js
-// @require      https://code.jquery.com/jquery-3.7.0.min.js#sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js#sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==
 // ==/UserScript==
 
 $(document).ready(function() {
