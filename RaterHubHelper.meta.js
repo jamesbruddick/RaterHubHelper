6d49507715a6c1b4dba57cf02b62d46d70a27c74
JamesTGH <26239426+JamesTGH@users.noreply.github.com>
2024-01-16 10:47:52 -0600

    Updated jQuery to 3.7.1 & Changed filenames

--- RaterHubHelper.meta.js
+++ RaterHubHelper.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         RaterHubHelper
-// @version      2.1.1
+// @version      2.1.2
 // @description  RaterHubHelper
 // @author       JamesTGH
 // @match        https://www.raterhub.com/*
-// @updateURL    https://tm.jamestgh.com/RaterHubHelper.meta.js
-// @downloadURL  https://tm.jamestgh.com/RaterHubHelper.user.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js#sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==
+// @updateURL    https://tm.jamestgh.com/raterhubhelper.meta.js
+// @downloadURL  https://tm.jamestgh.com/raterhubhelper.user.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js#sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==
 // ==/UserScript==
